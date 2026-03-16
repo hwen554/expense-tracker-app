f6504b97643ce2ef0c8194f50ce54ae466f3da5c
翁奉先 <a123456@123456deMacBook-Pro.local>
2026-03-16 16:50:36 +0800

    feat: add style

--- app/(modals)/transactionModal.tsx
+++ app/(modals)/transactionModal.tsx
@@ -4,12 +4,14 @@ import ImageUpload from "@/components/ImageUpload";
 import Input from "@/components/Input";
 import ModalWrapper from "@/components/ModalWrapper";
 import Typo from "@/components/Typo";
-import { colors, spacingY } from "@/constants/theme";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { useAuth } from "@/contexts/authContext";
 import { TransactionType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React, { useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
+
 
 const TransactionModal = () => {
   const { user, updateUserData } = useAuth();
@@ -59,9 +61,14 @@ const TransactionModal = () => {
               onSelect={(file) => 
                 setTransaction({ ...transaction, image: file})
               }
+              placeholder="Upload Image"
             />
           </View>
         </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+
       </View>
     </ModalWrapper>
   );
@@ -70,7 +77,110 @@ const TransactionModal = () => {
 export default TransactionModal;
 
 const styles = StyleSheet.create({
-  container: {},
-  inputContainer: {},
-  form: {},
+  container: {
+    flex: 1,
+    // justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+
+  },
+  inputContainer: {
+    gap: spacingY._10
+  },
+  form: {
+    gap: spacingY._20,
+    paddingVertical: spacingY._15,
+    paddingBottom: spacingY._40
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  iosDropDown: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: verticalScale(14),
+    borderWidth: 1,
+    color: colors.white,
+    borderColor: colors.neutral300,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15
+  },
+  androidDropDown: {
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    fontSize: verticalScale(14),
+    color: colors.white,
+    borderColor: colors.neutral300,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._5
+  },
+  dateInput: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    borderRadius: radius._10,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15
+  },
+  iosDatePicker: {
+
+  },
+  dropdownContainer: {
+    height: verticalScale(54),
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    paddingHorizontal: spacingX._15,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  dropdownItemText: {
+    color: colors.white
+  },
+  dropdownSelectedText: {
+    color: colors.white,
+    fontSize: verticalScale(14),
+  },
+  dropdownListContainer: {
+    backgroundColor: colors.neutral900,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+    paddingVertical: spacingY._7,
+    top: 5,
+    borderColor: colors.neutral500,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    elevation: 10
+  },
+  dropdownPlaceholder: {
+    color: colors.white
+  },
+  dropdownItemContainer: {
+    borderRadius: radius._15,
+    marginHorizontal: spacingX._7,
+  },
+  dropdownIcon: {
+    height: verticalScale(30),
+    tintColor: colors.white
+  }
 });
