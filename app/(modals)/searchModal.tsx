0cabebc6dc0723971ccc11adfe9e1eca8e624a74
Gavin Weng <wengh7930@gmail.com>
2026-03-27 23:31:01 +0800

    feat:complete searchmodal func

--- app/(modals)/searchModal.tsx
+++ app/(modals)/searchModal.tsx
@@ -1,97 +1,116 @@
-import { Alert, StyleSheet, Text, View, ScrollView } from 'react-native'
-import { UserDataType, WalletType } from '@/types';
-import React, { useState, useEffect } from 'react'
-import ModalWrapper from '@/components/ModalWrapper'
-import Typo from '@/components/Typo'
-import { useAuth } from '@/contexts/authContext';
-import { useLocalSearchParams, useRouter } from 'expo-router';
-import * as ImagePicker from "expo-image-picker";
-import Header from '@/components/Header';
-import BackButton from '@/components/BackButton';
-import { colors, spacingX, spacingY } from '@/constants/theme';
-import Input from '@/components/Input';
-import Button from '@/components/Button';
-import { updateUser } from '@/services/userService';
-import { scale, verticalScale } from '@/utils/styling';
-import ImageUpload from '@/components/ImageUpload';
-import { createOrUpdateWallet, deleteWallet } from '@/services/walletService';
-import * as Icons from 'phosphor-react-native';
-
-
+import BackButton from "@/components/BackButton";
+import Header from "@/components/Header";
+import Input from "@/components/Input";
+import ModalWrapper from "@/components/ModalWrapper";
+import TransactionList from "@/components/TransactionList";
+import Typo from "@/components/Typo";
+import { colors, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import useFetchData from "@/hooks/useFetchData";
+import { TransactionType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import { orderBy, where } from "firebase/firestore";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 const SearchModal = () => {
-  const {user, updateUserData } = useAuth();
+  const { user, updateUserData } = useAuth();
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-  
+
   const [search, setSearch] = useState("");
 
+  const constraints = [where("uid", "==", user?.uid), orderBy("date", "desc")];
 
+  const {
+    data: allTransactions,
+    error,
+    loading: transactionsLoading,
+  } = useFetchData<TransactionType>("transactions", constraints);
+  // console.log("all transactions: ", allTransactions.length);
 
-
-
+  const filteredTransactions = allTransactions.filter((item) => {
+    if(search.length > 1){
+      if(
+        item.category?.toLowerCase()?.includes(search.toLowerCase()) ||
+        item.type?.toLowerCase()?.includes(search.toLowerCase()) ||
+        item.description?.toLowerCase()?.includes(search.toLowerCase()) 
+    ) {
+      return true;
+     }
+      return false;
+    }
+    return true;
+  });
 
   return (
-    <ModalWrapper>
-        <View style={styles.container}>
-          <Header
-            title={"Search"}
-            leftIcon={<BackButton />}
-            style={{marginBottom: 20}}
-          />
+    <ModalWrapper style={{ backgroundColor: colors.neutral900 }}>
+      <View style={styles.container}>
+        <Header
+          title={"Search"}
+          leftIcon={<BackButton />}
+          style={{ marginBottom: 20 }}
+        />
 
-          {/* form */}
-          <ScrollView contentContainerStyle={styles.form}>
-              <View style={styles.inputContainer}>
-                  <Typo color={colors.neutral200} style={{ marginBottom: spacingY._10}}>
-                    Wallet Name
-                  </Typo>
-                  <Input
-                       placeholder="Search..."
-                       value={search}
-                       onChangeText={(value) => 
-                          setSearch(value)
-                       }
-                  />
-              </View>
-          </ScrollView>
-        </View>
+        {/* form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Typo
+              color={colors.neutral200}
+              style={{ marginBottom: spacingY._10 }}
+            >
+              Wallet Name
+            </Typo>
+            <Input
+              placeholder="Search..."
+              value={search}
+              placeholderTextColor={colors.neutral400}
+              containerStyle={{ backgroundColor: colors.neutral800 }}
+              onChangeText={(value) => setSearch(value)}
+            />
+          </View>
 
-        
+          <View>
+            <TransactionList
+              data={filteredTransactions}
+              loading={transactionsLoading}
+              emptyListMessage="No Transaction matches your search keyword"
+            />
+          </View>
+        </ScrollView>
+      </View>
     </ModalWrapper>
-  )
-}
+  );
+};
 
 export default SearchModal;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "space-between",
-      paddingHorizontal: spacingY._20
-    },
-    inputContainer: {
-      
-    },
-    form: {
-      gap: spacingY._30,
-      marginTop: spacingY._15
-    },
-    avatarContainer: {
-        position: "relative",
-        alignSelf: "center",
-    },
-    avatar: {
-      alignSelf: "center",
-      backgroundColor: colors.neutral300,
-      height: verticalScale(135),
-      width: verticalScale(135),
-      borderRadius: scale(999),
-      borderWidth: 1,
-      borderColor: colors.neutral500,
-    },
-    editIcon: {
-        position: "absolute",
-    }
-    
-})
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+  },
+  inputContainer: {},
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: scale(999),
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+  },
+  editIcon: {
+    position: "absolute",
+  },
+});
